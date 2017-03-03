60a8d6ba3eb993a6618b9cb24da8501fb262d829
leon.ho@adra.com <leon.ho@adra.com>
2017-03-03 15:47:31 +0100

    Added some updated information, added the link to the registration form, and added a few photos

--- ec/js/default.js
+++ ec/js/default.js
@@ -51,3 +51,49 @@ function googleMap() {
   });
 }
 google.maps.event.addDomListener(window, 'load', googleMap);
+
+// Countdown
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+// If today is before registration deadline, countdown to registration
+// If today is after registration deadline, countdown to camp start
+// If today is between camp dates, show "Easter Camp 2017 is on! See you there!"
+// If today is after camp, countdown to next Easter
+var deadline = new Date(Date.parse("2017-03-26"));
+initializeClock('clockdiv', deadline);
